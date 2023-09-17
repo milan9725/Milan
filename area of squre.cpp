9a2c4a849870c719bcb9afb36f06239d0a528ad0
milan9725 <parvadiyamilan9@gmail.com>
2023-09-17 21:14:29 +0530

    HTML

---
+++ area of squre.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	printf("\nenter the length of side:");
+	scanf("%d",&side);
+	area=side*side;
+	printf("\narea of square:%d",area);
+	return 0;
+}
