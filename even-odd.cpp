a9526df9059550f1ffe58785edf5e5ec3eac80dc
milan9725 <parvadiyamilan9@gmail.com>
2023-09-26 22:15:37 +0530

    HTML

---
+++ even-odd.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int num;
+	printf("enter the a number:");
+	scanf("%d",&num);
+	if("num%2==0")
+	{
+		printf("the number is even");
+	}
+	else
+	{
+		printf("the number is odd");
+	}
+}
