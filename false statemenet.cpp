7106f5dd448535709932865e6d52fa4a000b88b0
milan9725 <parvadiyamilan9@gmail.com>
2023-10-04 15:32:15 +0530

    HTML

---
+++ false statemenet.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int marks;
+	printf("enter your marks:");
+	scanf("%d",&marks);
+	if(marks>70)
+	{
+		printf("you will get a car"); //true statement
+	}
+	else
+	{
+		printf("you will get rickshaw"); //false statement
+	}
+}
