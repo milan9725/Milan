afe75e7f047bfe1ae510e3e0eee54a8ed3e03552
milan9725 <parvadiyamilan9@gmail.com>
2023-10-05 18:34:59 +0530

    HTML

---
+++ hw1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int b,h,a;
+	printf("enter the base:");
+	scanf("%d",&b);
+	printf("enter the hight:");
+	scanf("%d",&h);
+	a=b*h/2;
+	printf("area is:%d",a);
+	return 0;
+}
