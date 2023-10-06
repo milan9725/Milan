7a229d12c6da7969369e6e0e2d737d41e145c112
milan9725 <parvadiyamilan9@gmail.com>
2023-10-06 21:10:06 +0530

    HTML

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int age;
+	printf("enter the age:");
+	scanf("%d",&age);
+	if(age>18)
+	{
+		printf("you can vote");
+	}
+	else
+	{
+		printf("you cannot vote");
+	}
+}
---
+++ inline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+inline cube(int x)
+{
+	int result;
+	result=x*x*x;
+}
+main()
+{
+	int x=2;
+	cout<<cube(x);
+}
